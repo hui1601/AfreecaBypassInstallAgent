b67439d8cd1b1ce9bb95a3a716d396004a432d8a
Dong Heon Hee <hui1601@naver.com>
2024-05-01 10:32:59 +0900

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Afreeca Allow high resolution
+// @version      2024-05-01
+// @match        https://play.afreecatv.com/**
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=afreecatv.com
+// @grant        none
+// ==/UserScript==
+
+LivePlayer.getPlayerInfo().shouldConnectToAgentForHighQuality=()=>false;
