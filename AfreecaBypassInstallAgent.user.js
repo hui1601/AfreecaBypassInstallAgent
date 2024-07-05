48c012ea6b868240ede29952a4e9f5cdd5680678
Dong Heon Hee <hui1601@naver.com>
2024-07-06 03:55:14 +0900

    upgrade version

--- AfreecaBypassInstallAgent.user.js
+++ AfreecaBypassInstallAgent.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Afreeca Allow high resolution
-// @version      2024-05-01
+// @version      2024-07-06
 // @match        https://play.afreecatv.com/**
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=afreecatv.com
 // @grant        none
