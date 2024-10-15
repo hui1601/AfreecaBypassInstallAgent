85cc68c5f0d2c03c624c215bddc6f6f366a5c536
Dong Heon Hee <hui1601@naver.com>
2024-10-15 19:01:53 +0900

    Release 20241015

--- AfreecaBypassInstallAgent.user.js
+++ AfreecaBypassInstallAgent.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Afreeca Allow high resolution
-// @version      2024-07-06
+// @version      2024-10-15
 // @match        https://play.sooplive.co.kr/**
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=sooplive.co.kr
 // @grant        none
