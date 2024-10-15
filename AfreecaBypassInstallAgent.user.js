fbb2be21c097e5923aa231771b624a50ccd5eb15
Dong Heon Hee <hui1601@naver.com>
2024-10-15 19:01:13 +0900

    Update AfreecaBypassInstallAgent.user.js

--- AfreecaBypassInstallAgent.user.js
+++ AfreecaBypassInstallAgent.user.js
@@ -2,7 +2,7 @@
 // @name         Afreeca Allow high resolution
 // @version      2024-07-06
 // @match        https://play.sooplive.co.kr/**
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=afreecatv.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=sooplive.co.kr
 // @grant        none
 // ==/UserScript==
 
